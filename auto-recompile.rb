79779d61026326f1d85d5c4086439a05ee9358c1
David Backeus <duztdruid@gmail.com>
2013-05-03 11:16:31 +0200

    Ruby script to detect changes in streamio controls graphics and rebuild.

---
+++ auto-recompile.rb
@@ -0,0 +1,13 @@
+# Recompiles the streamio controls plugin whenever a new graphics swf is exported
+
+require 'bundler/setup'
+Bundler.require
+
+fsevent = FSEvent.new
+fsevent.watch "#{Dir.pwd}/plugins/controls/src/flash/streamio", latency: 2 do |dirs|
+  puts "#{Time.now} - Recompiling!"
+  system "ant -f plugins/controls/build-streamio.xml"
+  system "cp plugins/controls/build/flowplayer.controls-streamio* site/content/swf"
+  system "growlnotify -m 'Recompiled!'"
+end
+fsevent.run
